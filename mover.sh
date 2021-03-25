4504cee3d22d656cd1af67580ccf86471f6d6b85
Timofey Epanchintsev <t.epanchintsev@carbonsoft.ru>
2021-03-25 19:35:19 +0500

    Добавлено(mover.sh): вспомогательный скрипт для раскидывания картинок по папкам

---
+++ mover.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+declare daydir
+declare DATADIR="/opt/atom_webcam/data"
+
+for file in "$DATADIR"/*; do
+	daydir="$(date -d @$(echo $file | cut -d'.' -f2) +%d%m%y)"
+	echo $DATADIR/$daydir
+	mkdir -p "$DATADIR/$daydir"
+	mv "$file" "$DATADIR/$daydir"
+done
