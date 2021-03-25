987c5b7f06e87d3b1f26466b762190ec1263ae97
Timofey Epanchintsev <t.epanchintsev@carbonsoft.ru>
2021-03-25 20:18:52 +0500

    Исправлено(mover.sh): небольшое исправление

--- mover.sh
+++ mover.sh
@@ -3,7 +3,8 @@
 declare daydir
 declare DATADIR="/opt/atom_webcam/data"
 
-for file in "$DATADIR"/*; do
+for file in "$DATADIR"/*.jpg; do
+	[ ! -f "$file" ] && continue
 	daydir="$(date -d @$(echo $file | cut -d'.' -f2) +%d%m%y)"
 	echo $DATADIR/$daydir
 	mkdir -p "$DATADIR/$daydir"
