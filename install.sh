7ff6d27c1625c64f4c45106e7a34554f32c73a9e
Timofey Epanchintsev <t.epanchintsev@carbonsoft.ru>
2021-03-30 17:00:07 +0500

    Добавлено(веб-сервер): nginx deploy

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ declare WORKDIR="${4:-$(pwd)}"
 declare SAVE_PATH="${5:-$(pwd)/data}"
 
 systemctl stop atom_webcam.service &>/dev/null || true
+systemctl stop atom_web.service &>/dev/null || true
 
 export USER WORKDIR INTERVAL SAVE_PATH DEBUG
 envsubst '$USER,$WORKDIR,$DEBUG,$INTERVAL,$SAVE_PATH' <atom_webcam.service.tmplt > /etc/systemd/system/atom_webcam.service
@@ -23,7 +24,13 @@ chown "$USER:$USER" /var/log/atom_webcam.log
 
 envsubst '$USER' <logrotate.tmplt > /etc/logrotate.d/atom_webcam
 systemctl enable atom_webcam.service
+
+cp -av web/nginx.conf /etc/nginx/sites-available/atom_web.conf
+ln -s /etc/nginx/sites-available/atom_web.conf /etc/nginx/sites-enabled/
+systemctl enable atom_web.service
+
 systemctl start atom_webcam.service
+systemctl start atom_web.service
 
 echo "Проверьте, что все хорошо запустилось:"
 echo "sudo systemctl status atom_webcam.service"
