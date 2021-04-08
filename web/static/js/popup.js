62af79cabee340902773684cdf5cd82070920b55
Timofey Epanchintsev <t.epanchintsev@carbonsoft.ru>
2021-04-08 19:55:24 +0500

    Добавлено(вебка): перемещение по галерее

--- web/static/js/popup.js
+++ web/static/js/popup.js
@@ -7,13 +7,28 @@ $(function () {
         $(".img-show img").attr("src", src);
     });
 
-    $(".image_btns i.fa-times, .overlay").click(function () {
+    $(".next-image").click(function (){
+        let cur_src = $(".img-show img").attr("src")
+        $(".img_container img[src='"+cur_src+"']").parent().next().find('img').click()
+    })
+
+    $(".prev-image").click(function (){
+        let cur_src = $(".img-show img").attr("src")
+        $(".img_container img[src='"+cur_src+"']").parent().prev().find('img').click()
+    })
+
+    $(".close-image, .overlay").click(function () {
         $(".show").fadeOut();
     });
 
     $(document).on('keydown', function(e) {
-        if (e.keyCode === 27)
-            $("span, .overlay").click();
+        let image_controls = {
+            27: '.close-image',
+            37: '.next-image',
+            39: '.prev-image'
+        }
+        if (Object.keys(image_controls).includes(e.keyCode + "") && $(".show").is(":visible"))
+            $(image_controls[e.keyCode]).click()
     });
 
 });
