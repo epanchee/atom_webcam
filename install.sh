9c1b424ad3efd8e2e67c7b0c5dc1a7875e3d8c29
epanchee <epanchee@gmail.com>
2021-09-26 17:29:41 +0500

    Исправлено(деплой): зафиксил запуск systemd внутри pipenv

--- install.sh
+++ install.sh
@@ -13,6 +13,8 @@ declare DEBUG="${3:-}"
 declare WORKDIR="${4:-$(pwd)}"
 declare SAVE_PATH="${5:-$(pwd)/data}"
 
+pipenv install
+
 systemctl stop atom_webcam.service &>/dev/null || true
 systemctl stop atom_web.service &>/dev/null || true
 
@@ -26,7 +28,7 @@ envsubst '$USER' <logrotate.tmplt > /etc/logrotate.d/atom_webcam
 systemctl enable atom_webcam.service
 
 cp -av web/nginx.conf /etc/nginx/sites-available/atom_web.conf
-ln -s /etc/nginx/sites-available/atom_web.conf /etc/nginx/sites-enabled/
+ln -s /etc/nginx/sites-available/atom_web.conf /etc/nginx/sites-enabled/ || true
 systemctl enable atom_web.service
 
 systemctl start atom_webcam.service
